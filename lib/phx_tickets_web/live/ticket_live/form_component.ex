585689a6a2605c74f56ce796073d29c9588dedc4
Niraj Kulkarni <kulkarniniraj14@gmail.com>
2025-07-23 19:23:36 +0530

    Handle ticket type change event and update parent ticket visibility

--- lib/phx_tickets_web/live/ticket_live/form_component.ex
+++ lib/phx_tickets_web/live/ticket_live/form_component.ex
@@ -25,7 +25,7 @@ defmodule PhxTicketsWeb.TicketLive.FormComponent do
         options={@type_options} prompt="Select ticket type" />
         <.input field={@form[:status]} type="select" label="Status"
         options={@status_options} value={@ticket.status} />
-        <.input field={@form[:parent_id]} type="select" label="Parent"
+        <.input :if={@show_parent} field={@form[:parent_id]} type="select" label="Parent"
         options={@tickets} value={@ticket.parent_id || ""} prompt="Select parent ticket" />
 
         <:actions>
@@ -52,6 +52,7 @@ defmodule PhxTicketsWeb.TicketLive.FormComponent do
      |> assign(assigns)
      |> assign(:status_options, ["open", "in_progress", "closed"])
      |> assign(:type_options, ["Epic", "Story", "Task"])
+     |> assign(:show_parent, true)
      |> assign_new(:form, fn ->
        to_form(TC.change_ticket(ticket))
      end)
@@ -60,8 +61,25 @@ defmodule PhxTicketsWeb.TicketLive.FormComponent do
 
   end
 
+  # Handle ticket type change
+  @impl true
+  def handle_event("validate", %{
+    "_target" => ["ticket", "type"],
+    "ticket" => %{"type" => type} = ticket_params}, socket) do
+    # IO.inspect(type, label: "Ticket Type in validate")
+    changeset = TC.change_ticket(socket.assigns.ticket, ticket_params)
+
+    show_parent = if type == "Epic", do: false, else: true
+    {:noreply,
+      socket
+      |> assign(:show_parent, show_parent)
+      |> assign(:form, to_form(changeset, action: :validate))
+    }
+  end
+
   @impl true
   def handle_event("validate", %{"ticket" => ticket_params}, socket) do
+    IO.inspect(ticket_params, label: "Ticket Params in validate")
     changeset = TC.change_ticket(socket.assigns.ticket, ticket_params)
     {:noreply, assign(socket, form: to_form(changeset, action: :validate))}
   end
