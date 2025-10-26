d1576cdb08cbc80d86ff04fa427cc49fabfc5248
Niraj Kulkarni <kulkarniniraj14@gmail.com>
2025-10-26 08:39:20 +0530

    Add logger config for dev env

--- config/dev.exs
+++ config/dev.exs
@@ -63,7 +63,8 @@ config :phx_tickets, PhxTicketsWeb.Endpoint,
 config :phx_tickets, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "[$level] $metadata $message\n",
+  metadata: [:mfa]
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
